73b5be59e1aa5ef564042033c4d47b9c502204fa
fatimabenzaoui <fatima.benzaoui@yahoo.fr>
2024-05-08 16:12:42 +0200

    project init

---
+++ src/main/java/com/francis/demo/FrancisApplication.java
@@ -0,0 +1,13 @@
+package com.francis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francis/demo/FrancisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
